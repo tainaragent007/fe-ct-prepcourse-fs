6d513d4b115933d0f211f4ce2fc388a47fabb398
tainaragent007 <tainaragent007@gmail.com>
2023-01-12 23:00:01 -0300

    apunto de terminar

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -152,10 +152,10 @@ function operadoresLogicos(num1, num2, num3) {
       return "Error";} 
    else if(num1 < 0 || num2 < 0 || num3 < 0){
       return "Hay negativos";} 
+   else if(num1 > num2 && num1 > num3 && num1 > 0){
+      return "Número 1 es mayor y positivo";} 
    else if(num3 > num1 && num3 > num2){
       return num3+1;} 
-   else if(num1 > num2 && num1 > num3 && num1 > 0){
-         return "Número 1 es mayor y positivo";} 
    else {
       return false;}
 }

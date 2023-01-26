a180e3bbd72a78281cdeed1ce3c748fcfc8e7bed
tainaragent007 <tainaragent007@gmail.com>
2023-01-26 00:21:56 -0300

    terminando el m05

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -104,12 +104,12 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
-   if(arguments.length < 1) return 0; 
-   var total = 1;
-   for(var i = 0; i < arguments.length; i++) {
-      total = total * arguments[1];
-   }
-   return total;
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
 }
 
 function cuentoElementos(array) {
